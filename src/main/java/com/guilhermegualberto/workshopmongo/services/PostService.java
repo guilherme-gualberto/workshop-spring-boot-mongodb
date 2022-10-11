1174fce661c4de3227cc36543143e512754c78e1
Guilherme Gualberto <guilherme.moreira1997@hotmail.com>
2022-10-11 10:53:27 -0300

    Get a post by id

---
+++ src/main/java/com/guilhermegualberto/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.guilhermegualberto.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilhermegualberto.workshopmongo.domain.Post;
+import com.guilhermegualberto.workshopmongo.repository.PostRepository;
+import com.guilhermegualberto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!") );
+	}
+
+}
